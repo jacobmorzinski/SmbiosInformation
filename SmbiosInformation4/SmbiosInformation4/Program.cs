871f024e8ca45ffdf6a78d5c6b3f67d20a1b9862
Jacob Morzinski <jmorzins@mit.edu>
2018-01-19 00:43:01 -0500

    Add day 4

---
+++ SmbiosInformation4/SmbiosInformation4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmbiosInformation4
+{
+    class Program
+    {
+        static public void Main()
+        {
+            var serialNumber = Windows.System.Profile.SystemManufacturers.
+                               SmbiosInformation.SerialNumber;
+            System.Console.WriteLine($"Serial number = {serialNumber}");
+        }
+    }
+}
